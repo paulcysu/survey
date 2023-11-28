9cee93adf05750075e8d3416b0ff2315454cf25b
Paul Su <su.paul.cy@gmail.com>
2023-11-27 17:28:45 -0800

    add all files

---
+++ survey-frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('@tailwindcss/forms'),
+  ],
+}
+
